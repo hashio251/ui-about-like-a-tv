4b4fdff1801ce6d03ec1ae25f1e1dd97ce103b86
hashio.y <251zs03753@student.zen.ac.jp>
2026-07-17 16:46:58 +0900

    feat: add

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,30 +21,30 @@ const channelButtons = document.querySelectorAll(".button");
 // ======================
 // Channnels
 // ======================
-const firstViewImage =   "assets/images/loading_1.jpg";
+const firstViewImage =   "assets/images/loading/loading_1.jpg";
 
 const loadingImages = [
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_1.jpg",
-  "assets/images/loading_cage0.jpg",
-  "assets/images/loading_cage1.jpg",
-  "assets/images/loading_cage2.jpg",
-  "assets/images/loading_cage3.jpg",
-  "assets/images/loading_cage4.jpg",
-  "assets/images/loading_cage5.jpg",
-  "assets/images/loading_cage6.jpg",
-  "assets/images/loading_cage7.jpg",
-  "assets/images/loading_cage8.jpg",
-  "assets/images/loading_cage9.jpg",
-  "assets/images/loading_cage10.jpg",
-  "assets/images/loading_success.jpg",
-  "assets/images/loading_success.jpg",
-  "assets/images/loading_success.jpg"
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_1.jpg",
+  "assets/images/loading/loading_cage0.jpg",
+  "assets/images/loading/loading_cage1.jpg",
+  "assets/images/loading/loading_cage2.jpg",
+  "assets/images/loading/loading_cage3.jpg",
+  "assets/images/loading/oading_cage4.jpg",
+  "assets/images/loading/loading_cage5.jpg",
+  "assets/images/loading/loading_cage6.jpg",
+  "assets/images/loading/loading_cage7.jpg",
+  "assets/images/loading/loading_cage8.jpg",
+  "assets/images/loading/loading_cage9.jpg",
+  "assets/images/loading/loading_cage10.jpg",
+  "assets/images/loading/loading_success.jpg",
+  "assets/images/loading/loading_success.jpg",
+  "assets/images/loading/loading_success.jpg"
 ];
 
 const WELCOME_IMAGE = "assets/images/welcome_enjoy.jpg";
@@ -52,43 +52,43 @@ const WELCOME_IMAGE = "assets/images/welcome_enjoy.jpg";
 const channels = [
   {
     title: "ch1 about portfolio",
-    image: "https://as1.ftcdn.net/v2/jpg/16/35/56/20/1000_F_1635562014_fOnN9qkIkTRHt2CAtMZe9GUY7wiCpnyp.jpg",
+    image: "assets/images/ui/portfolio-thumbnail.jpg",
     url:"https://hashio251.github.io/index.html",
     description: "ch1 about portfolio"
   },
   {
     title: "ch2 about website",
-    image: "https://as2.ftcdn.net/v2/jpg/06/20/72/59/1000_F_620725999_DkEzjt1dDWSTGwSNNAo4xTN6TffQjSHa.jpg",
+    image: "assets/images/ui/website-shumbnail.jpg",
     url:"https://hashio251.github.io/frontend/website/index.html",
     description: "ch2 about website"
   },
   {
     title: "ch3 about collection",
-    image: "https://as2.ftcdn.net/v2/jpg/03/88/89/75/1000_F_388897532_oX60vTQtoqmjE77txvTOuaDWw8wXuVKY.jpg",
+    image: "assets/images/ui/collection-thumbnail.jpg",
     url:"https://hashio251.github.io/frontend/collection/index.html",
     description: "ch3 about collection"
   },
   {
     title: "ch4 about figma",
-    image: "https://as1.ftcdn.net/v2/jpg/20/61/14/80/1000_F_2061148019_zd8XXgFNqSnbL5tGeGOS084G2xCHNHyS.webp",
+    image: "assets/images/ui/figma-thumbnail.jpg",
     url:"https://www.figma.com/board/yDzidSITPcv3zB1YrIYiRK/hashio%E3%81%AE%E4%BD%9C%E5%93%81%E9%9B%86?node-id=6-212",
     description: "ch4 about figma"
   },
   {
     title: "ch5 about ps",
-    image: "https://as1.ftcdn.net/v2/jpg/20/14/36/62/1000_F_2014366236_59UNoyHJBU99C6x8lJTnoeAQNsb9kNp3.jpg",
+    image: "assets/images/ui/ps-thumbnail.jpg",
     url:"https://www.figma.com/board/yDzidSITPcv3zB1YrIYiRK/hashio%E3%81%AE%E4%BD%9C%E5%93%81%E9%9B%86?node-id=0-1",
     description: "ch5 about ps"
   },
   {
     title: "ch6 about ai",
-    image: "https://as2.ftcdn.net/v2/jpg/18/89/19/79/1000_F_1889197901_m4I9ilnwagK9L2UltSmCC4DWAOJc123W.jpg",
+    image: "assets/images/ui/ai-thumbnail.jpg",
     url:"https://www.figma.com/board/yDzidSITPcv3zB1YrIYiRK/hashio%E3%81%AE%E4%BD%9C%E5%93%81%E9%9B%86?node-id=3-234",
     description: "ch6 about ai"
   },
   {
     title: "ch7 about openprocessing",
-    image: "https://as1.ftcdn.net/v2/jpg/04/90/51/96/1000_F_490519655_gsswVAXQtfFwm76Ozz0geFoba9IoaZAT.jpg",
+    image: "assets/images/ui/op-shumbnail.jpg",
     url:"https://openprocessing.org/user/532184/#sketches",
     description: "ch7 about openprocessing"
   },
